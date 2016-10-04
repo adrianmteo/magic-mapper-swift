0e4ff2db44cd30b1ca89edeb6a012a61a845b0a3
Adrian Mateoaea <adrianitech@gmail.com>
2016-10-04 09:31:23 +0300

    `valueForKeyPath(_ keyPath: String)` now handles arrays

--- MagicMapper/Dictionary+Value.swift
+++ MagicMapper/Dictionary+Value.swift
@@ -9,17 +9,27 @@ extension Dictionary {
     
     public func valueForKeyPath(_ keyPath: String) -> Any? {
         var keys = keyPath.components(separatedBy: ".")
-        guard let first = keys.first as? Key else {
+        guard let key = keys.first as? Key else {
             print("Unable to use string as key on type: \(Key.self)")
             return nil
         }
         
-        guard let value = self[first] else { return nil }
+        guard var value = self[key] else { return nil }
         keys.remove(at: 0)
         
-        if !keys.isEmpty, let subDict = value as? Dictionary {
-            let rejoined = keys.joined(separator: ".")
-            return subDict.valueForKeyPath(rejoined)
+        if !keys.isEmpty {
+            while
+                let key = keys.first,
+                let index = Int(key),
+                let arr = value as? [Value],
+                index >= 0 && index < arr.count {
+                    keys.remove(at: 0)
+                    value = arr[index]
+            }
+            if let dict = value as? Dictionary {
+                let rejoined = keys.joined(separator: ".")
+                return dict.valueForKeyPath(rejoined)
+            }
         }
         
         return value
